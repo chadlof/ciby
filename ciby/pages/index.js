bff3d794de24910197ab1c4c055ce5fbc38a3e4e
chadlof <chad@chads-MacBook-Pro.local>
2020-11-30 00:01:31 -0600

    setup

---
+++ ciby/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Index = () => {
+  return (
+    <Wrapper>Hello World</Wrapper>
+  )
+}
+
+
+export default Index
